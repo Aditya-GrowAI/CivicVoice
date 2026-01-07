2ad977a3d2acc04699b4f32daea30360f9948d9a
prashanth <prashanthannaldas453@gmail.com>
2026-01-07 21:30:06 +0530

    Prashanth Updated

--- civic_voice_frontend/src/components/navBarHome.jsx
+++ civic_voice_frontend/src/components/navBarHome.jsx
@@ -1,13 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { NavLink, Link } from "react-router-dom";
-import Logo from '../assets/Logo.jpeg';
+import { NavLink, useNavigate } from "react-router-dom";
+import Logo from "../assets/Logo.jpeg";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function NavBarHome() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login", { replace: true });
+  };
+
   return (
     <div className="container-fluid px-4">
       <header className="navbar shadow-sm bg-white">
         <div className="row w-100 align-items-center">
-          <div className="col"><img className="logo" src={Logo} alt="logo" /></div>
+
+          <div className="col">
+            <NavLink to="/home">
+              <img className="logo" src={Logo} alt="logo" />
+            </NavLink>
+          </div>
 
           <div className="col-md-6 d-flex justify-content-center">
             <ul className="nav nav-pills">
@@ -21,7 +34,6 @@ function NavBarHome() {
                 <li className="nav-item" key={item.path}>
                   <NavLink
                     to={item.path}
-                    end={item.path === "/"}
                     className={({ isActive }) =>
                       "nav-link" + (isActive ? " active" : "")
                     }
@@ -32,44 +44,29 @@ function NavBarHome() {
               ))}
             </ul>
           </div>
+
           <div className="col-md-3 d-flex justify-content-end">
             <div className="dropdown">
-              <Link
-                to="#"
-                className="d-flex align-items-center text-dark text-decoration-none dropdown-toggle"
+              <button
+                className="btn btn-link text-dark dropdown-toggle p-0"
                 data-bs-toggle="dropdown"
-                aria-expanded="false"
-                role="button"
               >
-                <img
-                  src="https://github.com/mdo.png"
-                  alt="profile"
-                  width="32"
-                  height="32"
-                  className="rounded-circle me-2"
-                />
-                <strong>Prashanth</strong>
-              </Link>
+                <i className="bi bi-list fs-3"></i>
+              </button>
 
-              <ul className="dropdown-menu dropdown-menu-end text-small">
+              <ul className="dropdown-menu dropdown-menu-end">
                 <li>
-                  <Link className="dropdown-item" to="/profile">
-                    Profile
-                  </Link>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <button className="dropdown-item">
-                    <Link className="dropdown-item" to="/">
-                      Sign out
-                    </Link>
+                  <button
+                    className="dropdown-item"
+                    onClick={handleLogout}
+                  >
+                    Sign out
                   </button>
                 </li>
               </ul>
             </div>
           </div>
+
         </div>
       </header>
     </div>
